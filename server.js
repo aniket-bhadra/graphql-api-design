510f5413d2f48a35acdf6dde6ff5f467874f9192
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-03-11 14:23:55 +0530

    bsaic GraphQL server setup completed for revision of GraphQL

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
-const app = express();
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 
-app.use(cors());
-
-// Sample route
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const port = 8000;
+const server = new ApolloServer({
+  typeDefs: `type Query { hello: String }`,
+  resolvers: {
+    Query: {
+      hello: () => "hello world",
+    },
+  },
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+startStandaloneServer(server, {
+  listen: { port },
+})
+  .then(() => console.log(`Apollo Server running on http://localhost:${port}`))
+  .catch((error) => {
+    console.error("Failed to start the server:", error);
+  });
+
+//-------------------------express app
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Sample route
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
