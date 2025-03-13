3aced21984454e45522a7a1c7e69f9652bd6a374
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-03-13 11:35:42 +0530

    learned how to use LazyQuery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery, useMutation } from "@apollo/client";
 import React from "react";
 import { getUsers } from "./graphql/query/query";
 
 const App = () => {
-  const { loading, data, error } = useQuery(getUsers);
-  console.log(data);
+  // const { loading, data, error } = useQuery(getUsers);
+
+  const [trigger, { loading, error, data }] = useLazyQuery(getUsers);
+  // console.log(data);
 
   if (error) return <h1>something went wrong</h1>;
   return (
     <>
+    <button onClick={()=>trigger()}>show users</button>
       {loading ? (
         <p>Loading.....</p>
       ) : (
         <div>
-          {data.users.map((user) => (
+          {data?.users.map((user) => (
             <p key={user._id}>{user.name}</p>
           ))}
         </div>
